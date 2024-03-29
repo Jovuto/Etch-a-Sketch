56787466505a73f10d51235a47ace84f024e3f5e
Jovuto <153099429+Jovuto@users.noreply.github.com>
2024-03-29 08:22:49 +0000

    feat: add ability to color squares red

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ for (i = 0; i < 256; i++) {
     const PAINT_AREA = document.createElement("div");
     GRID_AREA.appendChild(PAINT_AREA);
     PAINT_AREA.classList.add("paintArea");
+    PAINT_AREA.addEventListener("mouseover", (e) => {
+        e.target.classList.add("red");
+    })
 }
 
 console.log(GRID_AREA.childElementCount);
